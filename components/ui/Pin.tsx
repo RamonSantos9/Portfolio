ba4c581046e39075de00ea40ec0a6732b0423df8
RamonSantos9 <ramonfishh@gmail.com>
2024-06-13 20:49:30 -0300

    arquivos consertados

--- components/ui/Pin.tsx
+++ components/ui/Pin.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-export const PinContainer = ({
+const PinContainer = ({
   children,
   className,
   containerClassName,
@@ -12,12 +12,14 @@ export const PinContainer = ({
   containerClassName?: string;
 }) => {
   return (
-    <div className={cn("relative group/pin z-50 cursor-pointer", containerClassName)}>
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-4 flex justify-start items-start rounded-2xl shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden">
-          <div className={cn("relative z-50", className)}>{children}</div>
+    <div className={cn("relative group pin z-50 cursor-pointer", containerClassName)}>
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className={cn("p-4 flex justify-start items-start rounded-2xl shadow-[0px 8px 16px rgba(0,0,0,0.4)] border border-white/[0.1] group-hover:pin:border-white/[0.2] transition duration-700 overflow-hidden", className)}>
+          <div className="relative z-50">{children}</div>
         </div>
       </div>
     </div>
   );
 };
+
+export { PinContainer };
